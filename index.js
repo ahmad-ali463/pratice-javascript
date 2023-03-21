b439a4fd661735433aa41bb5c750cbd81d2f4eaa
Ahmad Ali Asif <spyali463@gmail.com>
2023-03-21 17:08:55 +0500

    pratice java

---
+++ index.js
@@ -0,0 +1,18 @@
+// pratice variables
+
+
+var age = 20
+var height = 4.3
+var name = "Ahmad ali"
+
+// how to get these variables from RAM?
+console.log(name)
+
+// write an instructions code program software to calcuate 2+2 in js
+
+var program = 2+2;
+console.log(program);
+
+var num1 = 2;
+var num2 = 2;
+console.log(num1+num2)
